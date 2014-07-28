8058d53079b8be0217178c1fe6bd118d0efb8fcd
hirop4 <hirop4@gmail.com>
2014-07-28 18:08:05 +0900

    Include ImageMagick

--- app/controllers/selection_controller.rb
+++ app/controllers/selection_controller.rb
@@ -5,5 +5,55 @@ class SelectionController < ApplicationController
     redirect_to :selection_show
   end
   def show
+    positions = {
+      1  => { :x => 470, :y => 200, :name => '柿谷' }, 
+      2  => { :x => 430, :y => 100, :name => '香川' },
+      3  => { :x => 430, :y => 300, :name => '清武' },
+      4  => { :x => 330, :y =>  70, :name => '長友' }, 
+      5  => { :x => 330, :y => 330, :name => '内田' },
+      6  => { :x => 270, :y => 150, :name => '長谷部' },
+      7  => { :x => 270, :y => 250, :name => '遠藤' }, 
+      8  => { :x => 150, :y => 130, :name => '今野' }, 
+      9  => { :x => 150, :y => 200, :name => '吉田' }, 
+      10 => { :x => 150, :y => 270, :name => '森重' }, 
+      11 => { :x =>  40, :y => 200, :name => '川島' } 
+    }
+
+    image_positions = {
+      1  => { :x => 470, :y => 150, :path => 'public/images/kakitani.png' }, 
+      2  => { :x => 430, :y =>  50, :path => 'public/images/kagawa.png' },
+      3  => { :x => 430, :y => 250, :path => 'public/images/kiyotake.png' },
+      4  => { :x => 330, :y =>  20, :path => 'public/images/nagatomo.png' }, 
+      5  => { :x => 330, :y => 280, :path => 'public/images/uchida.png' },
+      6  => { :x => 270, :y => 100, :path => 'public/images/hasebe.png' },
+      7  => { :x => 270, :y => 200, :path => 'public/images/endo.png' }, 
+      8  => { :x => 150, :y =>  80, :path => 'public/images/konno.png' }, 
+      9  => { :x => 150, :y => 150, :path => 'public/images/yoshida.png' }, 
+      10 => { :x => 150, :y => 220, :path => 'public/images/morishige.png' }, 
+      11 => { :x =>  40, :y => 150, :path => 'public/images/kawashima.png' } 
+    }
+
+    ground = Magick::Image.read("public/images/ground.jpg").first
+
+    # conposite image
+    image_positions.each {|key, value|
+      logger.debug(value[:path])
+      player = Magick::Image.read(value[:path]).first
+      player.resize!(0.6)
+      ground = ground.composite(player, value[:x], value[:y], Magick::OverCompositeOp)
+    }
+    
+    # draw text
+#    dr = Magick::Draw.new
+#    dr.font = 'ヒラギノ丸ゴ-Pro-W4'
+#    dr.stroke('transparent')
+#    dr.fill('black')
+#    dr.pointsize = 18
+#    positions.each { |key, value|
+#      dr.text(value[:x], value[:y], value[:name])
+#    }
+#    dr.draw(ground)
+
+    send_data(ground.to_blob, :type => 'image/jpeg', :disposition => 'inline')
   end
 end
