14a20b26103fba922ab44984db6654f22aebffc6
hirop4 <hirop4@gmail.com>
2014-09-24 17:07:36 +0900

    Delete comment

--- app/controllers/selection_controller.rb
+++ app/controllers/selection_controller.rb
@@ -1,5 +1,7 @@
 class SelectionController < ApplicationController
 require 'redis'
+#    Redis.current.set('hoge', 'fuga')
+#    @keys = Redis.current.keys
 require 'RMagick'
   def index
   end
@@ -30,48 +32,4 @@ require 'RMagick'
       @type_to_players = type_to_players
     end
   end
-
-#  def show_text
-#    Redis.current.set('hoge', 'fuga')
-#    @keys = Redis.current.keys
-#
-#    positions = {
-#      1  => { :x => 470, :y => 200, :name => 'みのもんた' }, 
-#      2  => { :x => 430, :y => 100, :name => 'ベッカム' },
-#      3  => { :x => 430, :y => 300, :name => '竹内結子' },
-#      4  => { :x => 330, :y =>  70, :name => 'ピカチュウ' }, 
-#      5  => { :x => 330, :y => 330, :name => '指原' },
-#      6  => { :x => 270, :y => 150, :name => 'タモリ' },
-#      7  => { :x => 270, :y => 250, :name => 'さんま' }, 
-#      8  => { :x => 150, :y => 130, :name => '北野武' }, 
-#      9  => { :x => 150, :y => 200, :name => '三浦一良' }, 
-#      10 => { :x => 150, :y => 270, :name => 'ペレ' }, 
-#      11 => { :x =>  40, :y => 200, :name => 'トッティ' } 
-#    }
-#
-#    ground = Magick::Image.read("public/images/ground.jpg").first
-#
-#    # draw text
-#    dr = Magick::Draw.new
-#    dr.font = 'ヒラギノ丸ゴ-Pro-W4'
-#    dr.stroke('transparent')
-#    dr.fill('black')
-#    dr.pointsize = 18
-#    
-#    players = params[:players] || []
-#    (1..11).to_a.each {|num|
-#      position = positions[num]
-#      if players[num-1].nil?
-#        dr.fill('black')
-#        random_key = positions.keys.sample
-#        dr.text(position[:x], position[:y], positions[random_key][:name])
-#      else
-#        dr.fill('red')
-#        dr.text(position[:x], position[:y], players[num-1])
-#      end
-#    }
-#    dr.draw(ground)
-#    send_data(ground.to_blob, :type => 'image/jpeg', :disposition => 'inline')
-#  end
-
 end
