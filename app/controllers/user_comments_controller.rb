f2fefe5b8a1f04c912531e19bbcd0e004ed58334
hirop4 <hirop4@gmail.com>
2015-10-30 15:56:48 +0900

    Add validation for comment

--- app/controllers/user_comments_controller.rb
+++ app/controllers/user_comments_controller.rb
@@ -2,7 +2,7 @@ class UserCommentsController < BaseController
   def create
     image_id = params[:image_id].to_i
     comment = params[:comment]
-    if comment && comment.length > 0
+    if valid_comment?(comment)
       user_comment = UserComment.new(image_id: image_id, comment: comment)
       user_comment.save
     end
@@ -12,5 +12,13 @@ class UserCommentsController < BaseController
   def index
     @comments = UserComment.all.order(created_at: :desc)
     @comments = @comments.page(params[:page])
+  end
+  private
+  def valid_comment?(comment)
+    if comment && comment.length > 0 && comment !~ /href/
+      true
+    else
+      false
+    end
   end
 end
